ef070e085e395ae60d74cb25e96572cdbf0c0481
Toth-Gabor <upcgabor@gmail.com>
2019-04-25 11:35:10 +0200

    fetchPoem method

--- src/main/java/com/codecool/web/dao/database/DatabasePoemDao.java
+++ src/main/java/com/codecool/web/dao/database/DatabasePoemDao.java
@@ -2,9 +2,13 @@ package com.codecool.web.dao.database;
 
 import com.codecool.web.dao.PoemDao;
 import com.codecool.web.model.Poem;
+import com.codecool.web.model.User;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DatabasePoemDao extends AbstractDao implements PoemDao {
@@ -15,8 +19,17 @@ public class DatabasePoemDao extends AbstractDao implements PoemDao {
     
     @Override
     public List<Poem> findAll() throws SQLException {
-        return null;
+        String sql = "SELECT id, user_id, title, poem FROM poems";
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            List<Poem> poems = new ArrayList<>();
+            while (resultSet.next()) {
+                poems.add(fetchPoem(resultSet));
+            }
+            return poems;
+        }
     }
+
     
     @Override
     public List<Poem> findAllByUser(int userId) throws SQLException {
@@ -31,5 +44,14 @@ public class DatabasePoemDao extends AbstractDao implements PoemDao {
     @Override
     public Poem add(int id, int userId, String title, String poem) throws SQLException {
         return null;
+    }
+    
+    private Poem fetchPoem(ResultSet resultSet) throws SQLException {
+        
+        int id = resultSet.getInt("id");
+        int userId = resultSet.getInt("user_id");
+        String title = resultSet.getString("title");
+        String poem = resultSet.getString("poem");
+        return new Poem(id, userId, title, poem);
     }
 }
