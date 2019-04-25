195ad2178bd814e920d00ceac4d1401be89b4235
Toth-Gabor <upcgabor@gmail.com>
2019-04-25 14:31:39 +0200

    poemServlet

--- src/main/java/com/codecool/web/servlet/PoemServlet.java
+++ src/main/java/com/codecool/web/servlet/PoemServlet.java
@@ -1,4 +1,40 @@
 package com.codecool.web.servlet;
 
-public class PoemServlet {
+
+import com.codecool.web.dao.PoemDao;
+import com.codecool.web.dao.database.DatabasePoemDao;
+import com.codecool.web.dto.PoemDto;
+import com.codecool.web.model.Poem;
+import com.codecool.web.service.PoemService;
+import com.codecool.web.service.exception.ServiceException;
+import com.codecool.web.service.simple.SimplePoemService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet("/protected/poem")
+public class PoemServlet extends AbstractServlet {
+    
+    private static final String SQL_ERROR_CODE_UNIQUE_VIOLATION = "23505";
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try (Connection connection = getConnection(req.getServletContext())) {
+            PoemDao poemDao = new DatabasePoemDao(connection);
+            PoemService poemService = new SimplePoemService(poemDao);
+            
+            String id = req.getParameter("id");
+            
+            Poem poem = poemService.getPoemById(Integer.parseInt(id));
+            
+            sendMessage(resp, HttpServletResponse.SC_OK, new PoemDto(poem));
+        } catch (SQLException ex) {
+            handleSqlError(resp, ex);
+        }
+    }
 }
