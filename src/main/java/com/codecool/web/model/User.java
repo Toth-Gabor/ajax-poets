0e19b1617962d012290e3b06f2d18d4bc5a34cb5
Toth-Gabor <upcgabor@gmail.com>
2019-04-26 09:11:28 +0200

    add name to Use

--- src/main/java/com/codecool/web/model/User.java
+++ src/main/java/com/codecool/web/model/User.java
@@ -3,16 +3,22 @@ package com.codecool.web.model;
 import java.util.Objects;
 
 public final class User extends AbstractModel {
-
+    
+    private final String name;
     private final String email;
     private final String password;
 
-    public User(int id, String email, String password) {
+    public User(int id, String name, String email, String password) {
         super(id);
+        this.name = name;
         this.email = email;
         this.password = password;
     }
-
+    
+    public String getName() {
+        return name;
+    }
+    
     public String getEmail() {
         return email;
     }
@@ -27,12 +33,12 @@ public final class User extends AbstractModel {
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         User user = (User) o;
-        return Objects.equals(email, user.email) &&
+        return Objects.equals(name, user.name) && Objects.equals(email, user.email) &&
             Objects.equals(password, user.password);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), email, password);
+        return Objects.hash(super.hashCode(), name, email, password);
     }
 }
